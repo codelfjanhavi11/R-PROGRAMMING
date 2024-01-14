6897d791505e9e1aa807275a132f03b7f960c195
janhavi <janhaviprajapati.jp11@gmail.com>
2024-01-14 21:49:26 +0530

    created file

---
+++ loop assign.R
@@ -0,0 +1,23 @@
+#Assignment 15:
+  
+#  Note: Please use the while loop
+
+#Problem 1- Write your name 10 times.
+#Problem 2- Write the table of 9
+#Problem 3- Print all prime numbers between 1 to 100
+
+
+i<-1
+while(i<=10){
+  print("JANHAVI")
+  i=i+1
+}
+
+x<-1
+while(x<=10){
+  print(paste(9,'*',x,'=',9*x))
+  x=x+1
+}
+
+
+
